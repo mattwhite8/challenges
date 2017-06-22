8c08a21e61a9f8a6789be1d437727b7ed9b6175b
Matt White <mattwhite1337@gmail.com>
2017-06-22 16:45:01 -0400

    ran eslint on fizzBuzz

--- fizzbuzz/javascript/mattwhite8/fizzbuzz.js
+++ fizzbuzz/javascript/mattwhite8/fizzbuzz.js
@@ -1,21 +1,23 @@
-//we take in a num argument that will represent the length of the array returned
+// we take in a num argument that will represent the length of the array returned
 function fizzBuzz(num) {
-	//create an empty array that will store the output
-	var newArr = [];
-	//loop number of times equal to num argument 
-	for(var i = 1; i <= num; i++){
-		//check for most complicated situation first
-		if((i % 3 === 0) && (i % 5 === 0)){
-			newArr.push('FizzBuzz');
-		}else if (i % 5 === 0){
-			newArr.push('Buzz');
-		}else if (i % 3 === 0){
-			newArr.push('Fizz')
-		}else {
-			newArr.push(i);
-		}
-	}
-	//return the modified array
-	return newArr;
+  // create an empty array that will store the output
+  const newArr = [];
+  // loop number of times equal to num argument
+  for (let i = 1; i <= num; i += 1) {
+    // check for most compliated situation first
+    if (i % 3 === 0 && i % 5 === 0) {
+      newArr.push('FizzBuzz');
+    } else if (i % 5 === 0) {
+      newArr.push('Buzz');
+    } else if (i % 3 === 0) {
+      newArr.push('Fizz');
+    } else {
+      newArr.push(i);
+    }
+  }
+  // return the modified array
+  return newArr;
 }
 
+const fizzBuzzTest = fizzBuzz(5);
+console.log(fizzBuzzTest);
